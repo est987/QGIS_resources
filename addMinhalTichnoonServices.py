5f701933a50bc5f7ab1b6ceef7e15cee4cacfd12
est987 <43732852+est987@users.noreply.github.com>
2020-12-23 13:04:39 +0200

    Update addMinhalTichnoonServices.py

--- addMinhalTichnoonServices.py
+++ addMinhalTichnoonServices.py
@@ -6,6 +6,7 @@ based on this stackexchange answer: https://gis.stackexchange.com/questions/2966
 RUN IN QGIS PYTHON CONSOLE
 '''
 
+##FeatureServer
 services=['compilation_tmm_darom', 'compilation_tmm_haifa', 'compilation_tmm_merkaz','compilation_tmm_tzafonn','gaz_compilation','gvulot_retzef','plan_index','road_compilation', 
           'reka', 'tama35_hanchayot_svivatiot','TAMA_1','Tama_35_1','tma_14_b', 'tma_34_b_3', 'tmm_1_30', 'tmm_2_9', 'tmm_3_21', 'tmm_4_14',
           'tmm_5','tmm_5_5','tmm_6','train_compilation','ttl_all_blue_lines', 'vatmal_mitchamim_muchrazim','Xplan_2039','Xplan_NoKanam','Xplan']
@@ -15,4 +16,9 @@ for s in services:
     iface.reloadConnections()
 
 
+##Map Server
+services_m=['retzef_tamamim_ikari_raster']
 
+for s in services_m:
+    QgsSettings().setValue('qgis/connections-arcgismapserver/{}/url'.format(s), 'https://ags.iplan.gov.il/arcgisiplan/rest/services/PlanningPublic/{}/MapServer/'.format(s))
+    iface.reloadConnections()
